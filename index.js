ed1e83f2831606f5b04039fbd27b42e533eadcf5
Petko D. Petkov <pdp@Aluminum.local>
2014-01-08 17:33:53 +0000

    Initial support for websecurify make_request api.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ http.request = function (params, cb) {
     }
     if (!params.port) params.port = params.scheme == 'https' ? 443 : 80;
     
-    var req = new Request(new xhrHttp, params);
+    var req = new Request(websecurify, params);
     if (cb) req.on('response', cb);
     return req;
 };
@@ -41,39 +41,3 @@ http.get = function (params, cb) {
 
 http.Agent = function () {};
 http.Agent.defaultMaxSockets = 4;
-
-var xhrHttp = (function () {
-    if (typeof window === 'undefined') {
-        throw new Error('no window object present');
-    }
-    else if (window.XMLHttpRequest) {
-        return window.XMLHttpRequest;
-    }
-    else if (window.ActiveXObject) {
-        var axs = [
-            'Msxml2.XMLHTTP.6.0',
-            'Msxml2.XMLHTTP.3.0',
-            'Microsoft.XMLHTTP'
-        ];
-        for (var i = 0; i < axs.length; i++) {
-            try {
-                var ax = new(window.ActiveXObject)(axs[i]);
-                return function () {
-                    if (ax) {
-                        var ax_ = ax;
-                        ax = null;
-                        return ax_;
-                    }
-                    else {
-                        return new(window.ActiveXObject)(axs[i]);
-                    }
-                };
-            }
-            catch (e) {}
-        }
-        throw new Error('ajax not supported in this browser')
-    }
-    else {
-        throw new Error('ajax not supported in this browser');
-    }
-})();
